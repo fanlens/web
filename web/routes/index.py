b8ff8a9ae568deb5c80938c6cf2f43f74358df11
Christian Junker <chjdev@gmail.com>
2016-06-15 16:13:07 +0200

    final fixes and landing page redirect for demo

--- web/routes/index.py
+++ web/routes/index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, url_for
 
 index = Blueprint('index', __name__)
 
 
 @index.route('/')
 def root():
-    """main index.html"""
-    return render_template('landing/index.html')
+    return redirect(url_for('tagger.random_comments'), code=302)
+#    """main index.html"""
+#    return render_template('landing/index.html')
