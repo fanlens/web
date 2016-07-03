e0c1f4aaf44a8100462cc2bfb41a152c90b9ef20
Christian Junker <chjdev@gmail.com>
2016-07-03 16:46:17 +0200

    make landing page disableable

--- web/routes/index.py
+++ web/routes/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import render_template, Blueprint, request, flash, session, g
+from flask import render_template, Blueprint, request, flash, session, g, redirect, url_for
 from flask_mail import Message
+
+from config.env import Environment
 
 from web.model.contact import ContactForm
 from web.modules.mail import mail
@@ -11,10 +13,15 @@ from web.controller.tagger import TaggerController
 
 index = Blueprint('index', __name__)
 
+web_env = Environment('WEB')
+
 
 @index.route('/', methods=['GET', 'POST'])
 def root():
     """main index.html"""
+    if web_env.get('disable_landing', '0').lower() in ('1', 'true'):
+        return redirect(url_for('tagger.random_comments'))
+
     contact_form = ContactForm()
     if request.method == 'POST':
         if session.get('already_sent', False):
@@ -32,5 +39,4 @@ def root():
     random_comment, = TaggerController.get_random_comments(g.demo_user.id, count=1, with_entity=True,
                                                            with_suggestion=True,
                                                            sources={'ladygaga'})
-    print(random_comment)
     return render_template('landing/index.html', contact_form=contact_form, demo=random_comment)
