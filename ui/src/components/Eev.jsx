7e5627c252e33158c12bc6a16e84c8fee43774ee
Christian Junker <chjdev@gmail.com>
2017-01-27 14:10:04 +0100

    crossbrowser fix

--- ui/src/components/Eev.jsx
+++ ui/src/components/Eev.jsx
@@ -5,17 +5,18 @@ import MessageList from "./chat/MessageList.jsx";
 import "./Eev.css";
 
 const Eev = ({messages}) => (
-  <main id="eev" style={{display: 'flex', flexDirection: 'column', flex: '1 1 auto'}}>
+  <main id="eev" style={{display: 'flex', flexDirection: 'column', flex: '1 1 auto', overflow: 'hidden'}}>
       <Paper className="row center-xs"
              style={{zIndex: 1, display: 'flex', flex: '0 0 auto', flexDirection: 'row', backgroundColor: 'white'}}
+             rounded={false}
              zDepth={1}>
         <div className="col-md-10 col-xs-12">
           <ChatInput />
         </div>
       </Paper>
-      <div className="row center-sm">
+      <div className="row center-sm" style={{overflowY: 'scroll'}}>
         <div id="message-list" className="col-sm-8" style={{display: 'flex', flexDirection: 'column'}}>
-          <div className="row" style={{display: 'flex', flex: '1 1 auto', overflowY: 'scroll'}}>
+          <div className="row" style={{display: 'flex', flex: '1 1 auto'}}>
             <div className="col-xs-12">
               <MessageList/>
             </div>
--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import {IndexLink, Link} from "react-router";
+import Paper from "material-ui/Paper";
 import "./Footer.css";
 
 const Footer = () => (
-  <footer className="row">
-    <div className="col-xs-4 start-xs">
-      <ul id="footer-nav">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/team">Team</Link>
-        </li>
-        <li>
-          <Link to="/legal">Legal</Link>
-        </li>
-      </ul>
-    </div>
-    <div className="col-xs end-xs">
-      <div className="copyright">Copyright © Fanlens 2017. All Rights Reserved.</div>
-    </div>
+  <footer>
+    <Paper className="row" zDepth={1} style={{width: '100%', margin: 0, padding: '0.25em'}} rounded={false}>
+      <div className="col-xs-4 start-xs">
+        <ul id="footer-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/team">Team</Link>
+          </li>
+          <li>
+            <Link to="/legal">Legal</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="col-xs end-xs">
+        <div className="copyright">Copyright © Fanlens 2017. All Rights Reserved.</div>
+      </div>
+    </Paper>
   </footer>
 );
 
